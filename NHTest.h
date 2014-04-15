2a692735ccdc509aa994d91cb0f22b57229686ac
Nicholas Hsu <nicholas9@gmail.com>
2014-04-15 16:07:46 -0700

    added test object

---
+++ NHTest.h
@@ -0,0 +1,13 @@
+//
+//  NHTest.h
+//  Git Test
+//
+//  Created by mrhsu on 4/15/14.
+//  Copyright (c) 2014 Jibberish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NHTest : NSObject
+
+@end
---
+++ NHTest.m
@@ -0,0 +1,13 @@
+//
+//  NHTest.m
+//  Git Test
+//
+//  Created by mrhsu on 4/15/14.
+//  Copyright (c) 2014 Jibberish. All rights reserved.
+//
+
+#import "NHTest.h"
+
+@implementation NHTest
+
+@end
